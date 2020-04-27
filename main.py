b771aa5af54a9abe7932d91c09fe0c534f05ae4e
jhashekhar <shekhar09jha@gmail.com>
2020-04-27 21:28:16 +0530

    Added scheduler

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import pandas as pd
 import torch
+
 import argparse
 
 import sys
-sys.path.insert(0, '/Users/xanthate/GitHub/disaster-clf/src')
+sys.path.insert(0, '/content/src')
 
 import config
-from train import train_fn, eval_fn
+from train import train, eval
 from dataset import DisasterDataset
 from model import Model
 
 
-# add arguments
-# test it on colab --- how to run python program in colab/jupyter notebook
-# TODO:// add scheduler
-# TODO:// add option of gradient accumulation
+parser = argparse.ArgumentParser(
+    description='arguments for training and inference')
 
-parser = argparse.ArgumentParser(description='arguments for training and inference')
-
-parser.add_argument("-file_path", type=str, help="file path")
-# parser.add_argument("-mode", typt=str, help="'train', 'eval'")
-parser.add_argument("-epochs", type=int, help="Number of epochs")
-parser.add_argument("-lr", type=float, help="learning rate")
-parser.add_argument("-model", type=str, help="'bert', 'roberta', 'xlnet'")
-# parser.add_argument("-device", type=str, help="gpu, tpu, cpu")
-parser.add_argument("-bs", type=int, help="batch size")
+parser.add_argument(
+    "-file_path", type=str, help="file path")
+parser.add_argument(
+    "-epochs", type=int, help="Number of epochs")
+parser.add_argument(
+    "-model", type=str, help="'bert', 'roberta', 'distilbert', 'xlnet'")
+parser.add_argument(
+    "-optimizer", type=str, help="'Adam' and 'AdamW' ")
+parser.add_argument(
+    "-scheduler", type=str, help="'StepLR' and 'MultiStepLR'")
+parser.add_argument(
+    "-bs", type=int, help="batch size")
 
 
 args = parser.parse_args()
@@ -35,19 +37,28 @@ config.VALID_BATCH_SIZE = args.bs
 df = pd.read_csv(args.file_path)
 
 t = int(0.9 * len(df))
-train = df[:t]
-valid = df[t:]
+train_df = df[:t]
+valid_df = df[t:]
 
-trainset = DisasterDataset(train.text.values, train.target.values, args.model)
-validset = DisasterDataset(valid.text.values, valid.target.values, args.model)
+trainset = DisasterDataset(
+    train_df.text.values,
+    train_df.target.values,
+    args.model)
 
-trainloader = torch.utils.data.DataLoader(trainset,
-                                          batch_size=config.TRAIN_BATCH_SIZE,
-                                          shuffle=True)
+validset = DisasterDataset(
+    valid_df.text.values,
+    valid_df.target.values,
+    args.model)
 
-validloader = torch.utils.data.DataLoader(validset,
-                                          batch_size=config.VALID_BATCH_SIZE,
-                                          shuffle=False)
+trainloader = torch.utils.data.DataLoader(
+                                trainset,
+                                batch_size=config.TRAIN_BATCH_SIZE,
+                                shuffle=True)
+
+validloader = torch.utils.data.DataLoader(
+                                validset,
+                                batch_size=config.VALID_BATCH_SIZE,
+                                shuffle=False)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -57,14 +68,46 @@ if args.model == 'xlnet':
 if args.model == 'bert':
     model = Model[args.model](dropout=0.3).to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-criterion = torch.nn.CrossEntropyLoss()
+else:
+    model = Model[args.model](dropout=0.3).to(device)
+
+criterion = config.loss
+optimizer = config.optimizer[args.optimizer](model.parameters(), config.LR)
+
+
+if args.scheduler == 'steplr':
+    scheduler = config.scheduler['steplr'](optimizer, config.STEP_SIZE)
+
+if args.scheduler == 'multisteplr':
+    scheduler = config.scheduler['multisteplr'](optimizer, config.MILESTONES)
 
 
 def run():
     for epoch in range(args.epochs):
-        train_loss = train_fn(trainloader, model, criterion, optimizer, device)
-        val_loss = eval_fn(validloader, model, criterion, device)
+        if args.scheduler:
+            train_loss = train(
+                trainloader,
+                model,
+                criterion,
+                optimizer,
+                device,
+                scheduler=scheduler,
+                accumulation_step=config.ACCUMULATION)
+        else:
+            train_loss = train(
+                trainloader,
+                model,
+                criterion,
+                optimizer,
+                device,
+                accumulation_step=config.ACCUMULATION)
+
+        val_loss = eval(
+            validloader,
+            model,
+            criterion,
+            device)
+
         print(f"Epoch: {epoch+1}/{args.epochs}, train_loss: {train_loss:.5f}, val_loss: {val_loss:.5f}")
 
 
--- src/train.py
+++ src/train.py
@@ -2,42 +2,49 @@ import torch
 from tqdm import tqdm
 
 
-def train_fn(dataloader, model, criterion, optimizer, device, scheduler=None, accumulation_steps=None):
+def train(
+        dataloader,
+        model,
+        criterion,
+        optimizer,
+        device,
+        scheduler=None,
+        accumulation_step=None):
+
     model.train()
     epoch_loss = []
+    model.zero_grad()
 
     for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
         ids = data['ids'].to(device)
-        mask = data['mask'].to(device)
-        token_type_ids = data['token_type_ids'].to(device)
         targets = data['targets'].to(device)
 
-        model.zero_grad()
         output = model(ids)
-        #o1, o2 = model(ids)
-        #print(len(output))
-        #print(output.size())
-        #print(o1.size(), o2.size())
         loss = criterion(output, targets)
         epoch_loss.append(loss.item())
-        loss.backward()
-        optimizer.step()
-    
+
+        if (i+1) % accumulation_step == 0:
+            loss.backward()
+            optimizer.step()
+
+            if scheduler:
+                scheduler.step()
+
+            model.zero_grad()
+
     return sum(epoch_loss)/len(epoch_loss)
 
 
-def eval_fn(dataloader, model, criterion, device):
+def eval(dataloader, model, criterion, device):
     model.eval()
     epoch_loss = []
     with torch.no_grad():
         for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
             ids = data['ids'].to(device)
-            mask = data['mask'].to(device)
-            token_type_ids = data['token_type_ids'].to(device)
             targets = data['targets'].to(device)
 
             output = model(ids)
             loss = criterion(output, targets)
             epoch_loss.append(loss.item())
-        
-        return sum(epoch_loss)/len(epoch_loss)
+
+    return sum(epoch_loss)/len(epoch_loss)
